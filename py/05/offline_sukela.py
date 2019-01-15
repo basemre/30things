be226849cf82897bc57c697d228a36e0c69d56df
Emre Baş <emrebas2@ogr.cbu.edu.tr>
2019-01-15 22:19:09 +0300

    Reads each entry on the page

--- py/05/offline_sukela.py
+++ py/05/offline_sukela.py
@@ -1,29 +1,20 @@
 from selenium import webdriver
-from requests import get
-from requests.exceptions import RequestException
-from contextlib import closing
-from bs4 import BeautifulSoup
 
-driver = webdriver.Chrome('C://chromedriver.exe')
+driver = webdriver.Chrome('C://chromedriver.exe') #Declare an environment variable
 driver.get("https://eksisozluk.com")
 
 search_box = driver.find_element_by_id("search-textbox")
 search_box.clear()
 
-userInput = "mustafa kemal atatürk" #Başlık örneği.
+userInput = "mustafa kemal atatürk" #Header example
 search_box.send_keys(userInput)
 search_box.submit()
 
 sukela_url = driver.current_url + '?a=nice'
 driver.get(sukela_url)
 
-#dıştaki loop, entry sayfaları için
-for i in range(1,5):
-    #sayfadaki her entry için
-    for i in range(1,10):
-        print(driver.find_element_by_xpath('/html[1]/body[1]/div[3]/div[2]/div[2]/section[1]/div[1]/ul[1]/li['+str(i)+']/div[1]').get_property('innerText'))
+#print each entry on the first page, it will be edited and deleted. soon.
+for i in range(1,10):
+    print(driver.find_element_by_xpath('//*[@id="entry-item-list"]/li['+str(i)+']/div[1]').get_property('innerText'))
 
 driver.quit()
-
-#/html[1]/body[1]/div[3]/div[2]/div[2]/section[1]/div[1]/ul[1]/li[1]/div[1]
-#/html[1]/body[1]/div[3]/div[2]/div[2]/section[1]/div[1]/ul[1]/li[2]/div[1]
